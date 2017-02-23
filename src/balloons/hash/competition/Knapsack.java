b1a1fd8384b78e93f6b061c773d8c09e71fb1c66
aidayounesi <aida.younesi@gmail.com>
2017-02-23 20:14:31 +0000

    some changes again in arguments

--- src/balloons/hash/competition/Knapsack.java
+++ src/balloons/hash/competition/Knapsack.java
@@ -1,15 +1,15 @@
 package balloons.hash.competition;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
 
 public class Knapsack {
 	
 	// return list of videos should be in this cache
 	// list of videos and their values that are suitable for this cache
 	// array of sizes of all videos in our world
-    public static ArrayList<Integer> knapsack(int cacheSize, HashMap<Integer, Integer> videos, int videos_sizes[]) {
-        int N = videos_sizes.length; // Get the total number of items. Could be wt.length or val.length. Doesn't matter
+    public static ArrayList<Integer> knapsack(int cacheSize, Map<Integer, Integer> videos, int videos_sizes[]) {
+        int N = videos.size(); // Get the total number of items. Could be wt.length or val.length. Doesn't matter
         int[][] V = new int[N + 1][cacheSize + 1]; //Create a matrix. Items are in rows and weight at in columns +1 on each side
         //What if the knapsack's capacity is 0 - Set all columns at row 0 to be 0
         for (int col = 0; col <= cacheSize; col++) {
@@ -19,20 +19,25 @@ public class Knapsack {
         for (int row = 0; row <= N; row++) {
             V[row][0] = 0;
         }
-        for ( int item = 1; item <= N; item++) {
-//            //Let's fill the values row by row
-//            for ( int weight=1; weight <= cacheSize; weight++ ) { 
-//                //Is the current items weight less than or equal to running weight
-//                if ( videos.get(item-1) <= weight ) {
+        for (Map.Entry<Integer, Integer> entry : videos.entrySet())
+        {
+            System.out.println(entry.getKey() + "/" + entry.getValue());
+        }
+
+        for ( int item = 0; item < N; item++) {
+            //Let's fill the values row by row
+            for ( int weight = 0; weight < cacheSize; weight++ ) { 
+                //Is the current items weight less than or equal to running weight
+                if ( videos.get(item) <= weight ) {
 //                    //Given a weight, check if the value of the current item + value of the item that we could afford with the remaining weight
 //                    //is greater than the value without the current item itself
 //                    V[item][weight] = Math.max(val[item-1]+V[item-1][weight-wt[item-1]], V[item-1][weight]);
-//                }
-//                else {
-//                    //If the current item's weight is more than the running weight, just carry forward the value without the current item
-//                    V[item][weight]=V[item-1][weight];
-//                }
-//            }
+                }
+                else {
+                    //If the current item's weight is more than the running weight, just carry forward the value without the current item
+                    V[item][weight]=V[item-1][weight];
+                }
+            }
         }
 //        //Printing the matrix
 //        for (int[] rows : V) {
